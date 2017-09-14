ad759da0c87174f91ae9037dade9baf2f5c0c2d5
Janez Urevc <janez@janezurevc.name>
2017-09-14 14:09:23 +0200

    sdfsdfsdf

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./node_modules/gitbook-cli/bin/gitbook.js build
-./node_modules/gitbook-cli/bin/gitbook.js pdf
+xvfb-run ./node_modules/gitbook-cli/bin/gitbook.js pdf
 cp -r _book/* out/
 cp book.pdf out/latest.pdf
