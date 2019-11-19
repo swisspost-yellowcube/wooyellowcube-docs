3507c18be0031ba36731a4339397fe58c1c53f12
Miro Dietiker <miro.dietiker@md-systems.ch>
2019-11-19 16:01:50 +0100

    Clean deploy key usage.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,12 +32,7 @@ rm -rf out/**/* || exit 0
 # Run our compile script
 doCompile
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+# Load deploy key.
 chmod 600 scripts/deploy_key
 eval `ssh-agent -s`
 ssh-add scripts/deploy_key
